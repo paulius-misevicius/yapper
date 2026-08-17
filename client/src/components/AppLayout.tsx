9b7f871e3b93cb3ca44bf9b675f5da633c1b343f
spr0nk6 <paulius.misevicius@protonmail.com>
2026-08-17 15:22:03 +0300

    Add scroll position saving with React Router's Scroll Restoration

--- client/src/components/AppLayout.tsx
+++ client/src/components/AppLayout.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import Header from "./Header"
 import NavSidebar from "./NavSidebar"
-import { Outlet } from "react-router"
+import { Outlet, ScrollRestoration } from "react-router"
 
 export default function AppLayout() {
 
@@ -22,6 +22,9 @@ export default function AppLayout() {
             <NavSidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
             <main className="relative mt-(--header-height) flex flex-1 lg:ml-(--sidebar-nav-width) px-5 lg:px-10">
                 <Outlet />
+                <ScrollRestoration 
+                    getKey={location => location.pathname}
+                />
             </main>
         </>
     )
